d7f50ba451fa98fb9d297b482ee987bbcc6e28e6
Diogo Silva <nobregadiogos@gmail.com>
2023-05-01 20:48:53 +0100

    Login/Register pop up done. Minor changes on HTML part.

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,8 +1,31 @@
 //In the future, make this in another way
+/*
 function closePopUp() {
     document.getElementById("loginPopUp").style.display = "none";
 }
 
 function loginPopUp() {
     document.getElementById("loginPopUp").style.display = "block";
-}
+}*/
+
+const loginRegisterPopUp = document.querySelector('.loginRegisterPopUp');
+const loginLink = document.querySelector('.loginLink');
+const registerLink = document.querySelector('.registerLink');
+const loginPopUpButton = document.querySelector('.loginPopUpButton')
+const closeIcon = document.querySelector('.closeIcon');
+
+registerLink.addEventListener('click', ()=> {
+    loginRegisterPopUp.classList.add('active');
+});
+
+loginLink.addEventListener("click", ()=> {
+    loginRegisterPopUp.classList.remove('active');
+});
+
+loginPopUpButton.addEventListener('click', ()=> {
+    loginRegisterPopUp.classList.add('activePopUp');
+});
+
+closeIcon.addEventListener('click', ()=> {
+    loginRegisterPopUp.classList.remove('activePopUp');
+});
