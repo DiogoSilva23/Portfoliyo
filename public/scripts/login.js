dbec9101753dd0dc9c34555a29e7e98fd0e346fe
Diogo Silva <nobregadiogos@gmail.com>
2023-05-04 18:15:28 +0100

    Register done. Few changes on Login/SignUp popup.

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -14,6 +14,51 @@ const registerLink = document.querySelector('.registerLink');
 const loginPopUpButton = document.querySelector('.loginPopUpButton')
 const closeIcon = document.querySelector('.closeIcon');
 
+async function makeRequest(url, options) {
+    try {
+        const response = await fetch(url, options);
+        return response;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function signUp() {
+    const username = document.getElementById("registerUsername").value;
+    const email = document.getElementById("registerEmail").value;
+    const password = document.getElementById("registerPassword").value;
+    const user = {
+        username: username,
+        email: email,
+        password: password
+    }
+
+    const reply = await makeRequest("http://localhost:8000/signUp", {
+        method: "POST",
+        body: JSON.stringify(user),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+    })
+
+    json = await reply.json();
+    switch (reply.status) {
+        case 409:
+            {
+                document.getElementById("message").innerHTML = json.msg;
+                break;
+            }
+        case 400:
+            {
+                document.getElementById("message").innerHTML = json.msg;
+                break;
+            }
+        case 201:
+            {
+                document.getElementById("message").innerHTML = json.msg;
+                break;
+            }
+    }
+}
+
 registerLink.addEventListener('click', ()=> {
     loginRegisterPopUp.classList.add('active');
 });
--- server.js
+++ server.js
@@ -1,15 +1,82 @@
 /*
 const http = require('http')
-const fs = require('fs')
 const port = 8000
 */
+
 const express = require('express')
+const bcrypt = require('bcrypt')
+const fs = require('fs')
 const app = express()
+const bodyParser = require('body-parser')
+
+let users = require("./db/users.json")
 
 app.use(express.static('public'))
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/index.html', function(req, res, next) {
     res.sendFile('/index.html')
+})
+
+/*
+app.get('/users', (req, res) => {
+    res.json(users)
+})
+*/
+
+function existingUser(email) {
+    for (user of users) {
+        if (user.email === email) {
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+function write(fich, db) {
+    fs.writeFile(fich, JSON.stringify(db, null, 4), 'utf8', err => {
+        if (err) {
+            console.log(`Error writing file: ${err}`)
+        } else {
+            console.log('Wrote on file ' + fich); 
+        }
+    })
+}
+
+app.post('/signUp', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        if (!existingUser(req.body.email)) {
+            const newUser = {
+                username: req.body.username,
+                email: req.body.email,
+                password: hashedPassword
+            }
+            
+            if (newUser.password.length < 7) {
+                return res.status(400).send({
+                    msg: `Password must be at least 7 characters long!`
+                })
+            }
+            users.push(newUser)
+            write("./db/users.json", users);
+            return res.status(201).send ({
+                msg: `${username} was created.`
+            });
+        } else {
+            return res.status(409).send({
+                msg: `The user already exists. Try again.`
+            })
+        }
+    } catch {
+        res.status(500).send()
+    }
 })
 
 app.listen(8000)
