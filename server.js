fe5c14269387e340e01b5aa92f76ffdbd43ad18e
Armando Brandão <armandobrandao192@gmail.com>
2023-05-15 22:17:19 +0100

    Some token bugs

--- server.js
+++ server.js
@@ -104,8 +104,7 @@ app.get("/posts", authenticateToken, (req, res) => {
 })
 
 app.post('/signUp', async (req, res) => {
-    try {
-        const salt = await bcrypt.genSalt()
+    const salt = await bcrypt.genSalt()
         const hashedPassword = await bcrypt.hash(req.body.password, salt)
         if (!existingUser(req.body.email)) {
             const newUser = {
@@ -122,13 +121,15 @@ app.post('/signUp', async (req, res) => {
             users.push(newUser)
             write("./db/users.json", users);
             return res.status(201).send ({
-                msg: `${username} was created.`
+                msg: `${newUser.username} was created.`
             });
         } else {
             return res.status(409).send({
                 msg: `The user already exists. Try again.`
             })
         }
+    try {
+        
     } catch {
         res.status(500).send()
     }
@@ -138,23 +139,25 @@ app.post('/signUp', async (req, res) => {
 app.post('/login', async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    try {
-        for (user of users) {
-            if (user.email === email) {
-                if (await bcrypt.compare(password, user.password)) {
-                    const accessToken = generateAccessToken(user)
-                    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
-                    res.json({ accessToken: accessToken, refreshToken: refreshToken })
-                    token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
-                    return res.status(201).json({
-                        auth: true,
-                        token: token,
-                    })
-                } else {
-                    return res.status(401).json({ msg: `Invalid Password!` })
-                }
+    for (user of users) {
+        if (user.email === email) {
+            if (await bcrypt.compare(password, user.password)) {
+                const accessToken = generateAccessToken(user)
+                const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+                res.json({ accessToken: accessToken, refreshToken: refreshToken })
+                token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+                return res.status(201).json({
+                    auth: true,
+                    token: token,
+                })
+            } else {
+                return res.status(401).json({ msg: `Invalid Password!` })
             }
         }
+    }
+    return res.status(404).json({ msg: `User not found!` })
+    try {
+       
         return res.status(404).json({ msg: `User not found!` })
     } catch {
         res.status(500).send();
