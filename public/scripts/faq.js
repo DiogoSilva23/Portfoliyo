242115828a272d1b0e7d1d37b547aaf9043b8ab9
AndreSantos02 <71610448+AndreSantos02@users.noreply.github.com>
2023-06-17 12:51:18 +0100

    Frontend improvements

--- public/scripts/faq.js
+++ public/scripts/faq.js
@@ -3,12 +3,13 @@ var faqQuestions = document.querySelectorAll('.faq-question');
 
 // Adicionar um evento para cada questão
 faqQuestions.forEach(function(question) {
-question.addEventListener('click', function() {
+    question.addEventListener('click', function() {
     // Mudar a visibilidade da resposta
     var answer = this.nextElementSibling;
-    answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
-
-    // Adicionar e/ou remover a classe ativa da questão
-    this.classList.toggle('active');
-});
+    if (answer.style.display === 'none' || answer.style.display === '') {
+      answer.style.display = 'block';
+    } else {
+      answer.style.display = 'none';
+    }
+  });
 });
