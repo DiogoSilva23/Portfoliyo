86259ca5297ee7a68c6b016d9e22fd0457805976
Diogo Silva <nobregadiogos@gmail.com>
2023-06-17 12:18:06 +0100

    Sign Up Enterprise FrontEnd almost done.

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -1,10 +1,11 @@
 const textarea = document.querySelector('textarea');
 const description = document.querySelector('.description');
+const descriptionEnterprise = document.querySelector('.descriptionEnterprise');
 
 textarea.addEventListener('keyup', e => {
     textarea.style.height = "auto";
     description.style.height = "auto";
+    descriptionEnterprise.style.height = "auto";
     let sHeight = e.target.scrollHeight;
     textarea.style.height = `${sHeight}px`;
-
 });
